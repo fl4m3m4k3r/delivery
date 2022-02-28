6c34d0d67659c466e7dcf0f8d5e4c40ca431cde1
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2022-02-28 18:13:07 +0300

    feat: create user using devise. User have a role
    User role can be: user, pro, operator, admin. User role by default: user

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# User which can be: user, pro, operator, admin
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  enum role: { user: 0, pro: 1, operator: 2, admin: 3 }
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+end
